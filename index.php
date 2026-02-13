3a56c69b3a960f3a42d143e9e91b9de5357a1a1d
SaraPG <sperezg16@gmail.com>
2026-02-13 18:33:08 +0100

    awa
    web

--- index.php
+++ index.php
@@ -49,6 +49,7 @@
 
             <div class="carousel-item active">
                 <img src="fotos/mundo.webp" class="d-block w-100 carousel-img" alt="Mundo">
+                <br>
                 <!-- TEXTO CENTRAL -->
                 <div class="conocimiento d-flex flex-column justify-content-center align-items-center text-center">
                     <h1>Explora el conocimiento</h1>
@@ -79,38 +80,38 @@
     </div>
 </main>
 
-    <footer class="custom-footer">
-        <div class="container">
-            <div class="row text-center text-md-start">
+<footer class="custom-footer">
+    <div class="container">
+        <div class="row text-center text-md-start">
 
-                <div class="col-md-4 mb-4">
-                    <h5 class="footer-title">WikiÁgora</h5>
-                    <p>Enciclopedia colaborativa moderna para explorar y compartir conocimiento.</p>
-                </div>
-
-                <div class="col-md-4 mb-4">
-                    <h5 class="footer-title">Enlaces</h5>
-                    <ul class="footer-links">
-                        <li><a href="#">Inicio</a></li>
-                        <li><a href="#">Artículos</a></li>
-                        <li><a href="#">Categorías</a></li>
-                        <li><a href="#">Sobre nosotros</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-4 mb-4">
-                    <h5 class="footer-title">Equipo</h5>
-                    <p>Aitor, Alberto, David y Sara</p>
-                </div>
-
+            <div class="col-md-4 mb-4">
+                <h5 class="footer-title">WikiÁgora</h5>
+                <p>Enciclopedia colaborativa moderna para explorar y compartir conocimiento.</p>
             </div>
 
-            <hr class="footer-divider">
-
-            <div class="text-center small">
-                © 2026 WikiÁgora | Todos los derechos reservados
+            <div class="col-md-4 mb-4">
+                <h5 class="footer-title">Enlaces</h5>
+                <ul class="footer-links">
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Artículos</a></li>
+                    <li><a href="#">Categorías</a></li>
+                    <li><a href="#">Sobre nosotros</a></li>
+                </ul>
             </div>
+
+            <div class="col-md-4 mb-4">
+                <h5 class="footer-title">Equipo</h5>
+                <p>Aitor, Alberto, David y Sara</p>
+            </div>
+
         </div>
-    </footer>
+
+        <hr class="footer-divider">
+
+        <div class="text-center small">
+            © 2026 WikiÁgora | Todos los derechos reservados
+        </div>
+    </div>
+</footer>
 
 </html>
