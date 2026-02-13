40a40c6ea84924a40394d8055376d179c4cd2336
aitorem <aitorbuenadicha@gmail.com>
2026-02-13 17:11:43 +0100

    AAA
    AA

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
             <a href="#">Pricing</a>
             <a href="#">FAQs</a>
             <a href="#">About</a>
-            <p>HOLAaaaaa
+            
         </nav>
 
         <div class="nav-actions">
