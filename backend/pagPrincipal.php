848832e70c8d01798e248a93120d300f6e9f8358
aitorem <aitorbuenadicha@gmail.com>
2026-02-14 14:35:01 +0100

    Arreglo rutas header.php y footer.php en pagPrincipal

--- backend/pagPrincipal.php
+++ backend/pagPrincipal.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/../includes/conexion.php';
-include 'includes/header.php';
+include '../includes/header.php';
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +16,7 @@ include 'includes/header.php';
 <body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<?php include 'includes/footer.php'; ?>
+<?php include '../includes/footer.php'; ?>
 </body>
 
 </html>
