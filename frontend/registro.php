e1bcbefef812ddd72bbfdad7072235f6f64c470b
David2005c <dgarciab37@educarex.es>
2026-02-18 19:35:48 +0100

    errores corregidos

--- frontend/registro.php
+++ frontend/registro.php
@@ -10,7 +10,7 @@ include '../includes/header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wikipedia - Registro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/index.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body class="bg-light">
